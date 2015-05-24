565fc0ec3ef6e0d2b3db11aa51ccab03457d200a
Mikey McCormack <pair+mmccormack@pivotal.io>
2015-05-24 11:36:06 -0700

    Whoops, make jasmine tests pass for real

--- spec/javascript/views/first_test.js
+++ spec/javascript/views/first_test.js
@@ -1,7 +1,7 @@
 describe('the main page', function() {
   it('has a button that says Create New Budget', function() {
     this.injectDependencies('renderTemplate');
-    view = this.renderTemplate('budget_view.html');
+    view = this.renderTemplate('budget.html');
     expect(view).toContainText('Monthly Expenses');
   });
 });
