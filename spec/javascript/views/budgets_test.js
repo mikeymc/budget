eb4a9ae9b38d0855ac1f3298a41af457ba3f5eaa
Mikey McCormack <pair+mmccormack@pivotal.io>
2015-07-03 22:03:40 -0700

    [#95570466] Clean up budgets_test.js

--- spec/javascript/views/budgets_test.js
+++ spec/javascript/views/budgets_test.js
@@ -1,5 +1,5 @@
 describe('The Budgets Page', function() {
-  var view, self, budgetSummaryRepositorySpy;
+  var view, self, budgetSummaryRepositorySpy, budgets;
 
   beforeEach(function() {
     this.injectDependencies('renderTemplate', 'Form', '$location', 'Http', 'BudgetSummaryRepository', '$state');
@@ -23,53 +23,62 @@ describe('The Budgets Page', function() {
 
     describe('clicking it', function() {
       it('sends the user to a New Budget page', function() {
-        self.Form.click(view, 'new_budget_button');
-        expect(self.$state.current.name).toEqual('new-budget');
+        this.Form.click(view, 'new_budget_button');
+        expect(this.$state.current.name).toEqual('new-budget');
       });
     });
   });
 
   it('contains a list entitled "Open Budgets"', function() {
-    view = render(self.$scope);
+    view = render(this.$scope);
     expect(view).toContainText('Open Budgets');
   });
 
   describe('the list of open budgets', function() {
     it('has zero items when this list is empty', function() {
-      self.$scope.budgets = [];
-      view = render(self.$scope);
-      expect(view.find('.open_budget_list_item').length).toBe(0);
+      budgets = {budgets: []};
+      budgetSummaryRepositorySpy.and.returnValue(this.Http.succeedToMakeRequest(budgets));
+
+      view = render(this.$scope);
+
+      expect(view.find('.budget').length).toBe(0);
     });
 
     it('has three items when the list is size three', function() {
-      var budgets = {budgets: [
+      budgets = {budgets: [
         {id:'123', name:'budget-one'},
         {id:'456', name:'budget-two'},
         {id:'789', name:'budget-three'}
       ]};
-      budgetSummaryRepositorySpy.and.returnValue(self.Http.succeedToMakeRequest(budgets));
-      view = render(self.$scope);
+      budgetSummaryRepositorySpy.and.returnValue(this.Http.succeedToMakeRequest(budgets));
+
+      view = render(this.$scope);
+
       expect(view.find('.budget').length).toBe(3);
     });
 
     it('shows the name of each budget in each item', function() {
-      var budgets = {budgets: [
+      budgets = {budgets: [
         {id:'123', name:'budget-one'},
         {id:'456', name:'budget-two'}
       ]};
-      budgetSummaryRepositorySpy.and.returnValue(self.Http.succeedToMakeRequest(budgets));
-      view = render(self.$scope);
+      budgetSummaryRepositorySpy.and.returnValue(this.Http.succeedToMakeRequest(budgets));
+
+      view = render(this.$scope);
+
       expect(view.find('.budget')[0]).toContainText('budget-one');
       expect(view.find('.budget')[1]).toContainText('budget-two');
     });
 
     it('sets the name attribute to the id of the budget', function() {
-       var budgets = {budgets: [
+      budgets = {budgets: [
         {id:'123', name:'budget-one'},
         {id:'456', name:'budget-two'}
       ]};
-      budgetSummaryRepositorySpy.and.returnValue(self.Http.succeedToMakeRequest(budgets));
-      view = render(self.$scope);
+      budgetSummaryRepositorySpy.and.returnValue(this.Http.succeedToMakeRequest(budgets));
+
+      view = render(this.$scope);
+
       expect(view.find('.budget:nth(0)').attr('name')).toEqual('123');
       expect(view.find('.budget:nth(1)').attr('name')).toEqual('456');
     });
@@ -77,28 +86,26 @@ describe('The Budgets Page', function() {
 
   describe('how the list of budget summaries is populated', function() {
     it('gets them from the BudgetSummaryRepository', function() {
-      var summaries = [];
-      summaries.push({id:'first-id', name:'first-budget'});
-      summaries.push({id:'second-id', name:'second-budget'});
-      budgetSummaryRepositorySpy.and.returnValue(self.Http.succeedToMakeRequest({budgets: summaries}));
-      view = render(self.$scope);
-      expect(self.BudgetSummaryRepository.all).toHaveBeenCalled();
+      budgets = [{id:'first-id', name:'first-budget'}, {id:'second-id', name:'second-budget'}];
+      budgetSummaryRepositorySpy.and.returnValue(this.Http.succeedToMakeRequest({budgets: budgets}));
+
+      view = render(this.$scope);
+
+      expect(this.BudgetSummaryRepository.all).toHaveBeenCalled();
       expect(view).toContainText('first-budget');
       expect(view).toContainText('second-budget');
     });
   });
 
   describe('clicking one of the budgets in the list', function() {
-    beforeEach(function() {
-      var budgets = [{id: '123', name: 'my-test-budget'}];
-      budgetSummaryRepositorySpy.and.returnValue(self.Http.succeedToMakeRequest({budgets: budgets}));
-      view = render(this.$scope);
-    });
-
     it('takes the user to budgets/{id-of-the-budget}', function() {
-      self.Form.click(view, '123');
-      self.$scope.$digest();
-      expect(self.$location.url()).toEqual('/budgets/123');
+      budgets = [{id: '123', name: 'my-test-budget'}];
+      budgetSummaryRepositorySpy.and.returnValue(this.Http.succeedToMakeRequest({budgets: budgets}));
+      view = render(this.$scope);
+
+      this.Form.click(view, '123');
+
+      expect(this.$location.url()).toEqual('/budgets/123');
     });
   });
 
