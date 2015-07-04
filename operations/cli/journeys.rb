b63fc0a77c459477dd6230ea846d0903d3a46ab5
Mikey McCormack <pair+mmccormack@pivotal.io>
2015-07-03 22:10:21 -0700

    [#95333144] Rename make_budget_spec to journey_test

--- operations/cli/journeys.rb
+++ operations/cli/journeys.rb
@@ -2,7 +2,7 @@ module CLI
   class Journeys < Thor
     desc 'test', 'Run journey tests'
     def test
-      system 'rspec ./spec/journeys/*_spec.rb'
+      system 'rspec ./spec/journeys/*_test.rb'
     end
   end
 end
