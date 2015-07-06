0198893ab864b3caac3c568ce3f366834fed81c2
Mikey McCormack <pair+mmccormack@pivotal.io>
2015-07-05 22:16:17 -0700

    [#95333144] Cleanly handle null incomes coming back from GET requests on the income page

--- app/assets/javascripts/angular-app/controllers/budget_controller.js
+++ app/assets/javascripts/angular-app/controllers/budget_controller.js
@@ -5,6 +5,10 @@ budget.controller('BudgetController', function($scope, $stateParams, BudgetRepos
 
   BudgetRepository.get($stateParams.budgetId).success(function(returnedBudget) {
     $scope.budget = returnedBudget.financial_budget;
-    $scope.budget.income.gross_annual_salary = parseFloat($scope.budget.income.gross_annual_salary);
+    if ($scope.budget.income != null) {
+      $scope.budget.income.gross_annual_salary = parseFloat($scope.budget.income.gross_annual_salary);
+    } else {
+      $scope.budget.income = {};
+    }
   });
 });
--- spec/javascript/views/budget_test.js
+++ spec/javascript/views/budget_test.js
@@ -3,19 +3,34 @@ describe('The Budget Page', function() {
 
   beforeEach(function() {
     this.injectDependencies('$scope', 'renderTemplate', 'Form', 'BudgetRepository', 'IncomeRepository', 'stub', 'Http', '$stateParams');
-    var budget = {financial_budget: {id: 1, name: 'my-budget', income: {gross_annual_salary: '123456.78'}}};
-    this.stub(this.BudgetRepository, 'get').withParam('budget-id').andReturn(this.Http.succeedToMakeRequest(budget));
     this.$stateParams.budgetId = 'budget-id';
-    view = this.renderTemplate('budget.html', this.$scope);
+    budget = {financial_budget: {id: 1, name: 'my-budget', income: {gross_annual_salary: '123456.78'}}};
   });
 
   describe('setting up the view', function() {
     it('fetches the budget from the server', function() {
+      this.stub(this.BudgetRepository, 'get').withParam('budget-id').andReturn(this.Http.succeedToMakeRequest(budget));
+      view = this.renderTemplate('budget.html', this.$scope);
       expect(view.find('input[name="gross_annual_salary"]').val()).toEqual('123456.78');
+    });
+
+    describe('when no income data yet exists', function() {
+      it('sets the annual salary text field to empty', function() {
+        budget = {financial_budget: {id: 1, name: 'my-budget', income: null}};
+        this.stub(this.BudgetRepository, 'get').withParam('budget-id').andReturn(this.Http.succeedToMakeRequest(budget));
+        view = this.renderTemplate('budget.html', this.$scope);
+
+        expect(view.find('input[name="gross_annual_salary"]').val()).toEqual('');
+      });
     });
   });
 
   describe('the Income section', function() {
+    beforeEach(function() {
+      this.stub(this.BudgetRepository, 'get').withParam('budget-id').andReturn(this.Http.succeedToMakeRequest(budget));
+      view = this.renderTemplate('budget.html', this.$scope);
+    });
+
     it('has a header that says Income', function() {
       expect(view).toContainText('Income');
     });
@@ -26,11 +41,14 @@ describe('The Budget Page', function() {
   });
 
   it('has an Expenses section', function() {
+    this.stub(this.BudgetRepository, 'get').withParam('budget-id').andReturn(this.Http.succeedToMakeRequest(budget));
+    view = this.renderTemplate('budget.html', this.$scope);
     expect(view).toContainText('Expenses');
   });
 
   describe('saving the information in the form', function() {
     it('saves the values the user entered', function() {
+      this.stub(this.BudgetRepository, 'get').withParam('budget-id').andReturn(this.Http.succeedToMakeRequest(budget));
       spyOn(this.IncomeRepository, 'save');
 
       view = this.renderTemplate('budget.html', this.$scope);
